f59979710f2464115b7177a819f8d8fc2e56bcbd
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-07-18 21:41:25 +0100

    clean up server jazz

--- server/src/graphql/index.js
+++
@@ -1,8 +0,0 @@
-import schema from './schema';
-import parseGraphQLHTTP from 'parse-graphql-server';
-
-export default {
-  setup(app, graphiql = false) {
-    app.use('/graphql', parseGraphQLHTTP({ schema, graphiql }));
-  },
-};
