2d9f2b187094155dbbacfe2a2209725bfb267a96
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-07-28 19:04:02 +0100

    fix app tests for when 3rd party modules are involved

--- baker/generators/app/templates/src/tests.js
+++ baker/generators/app/templates/src/tests.js
@@ -1,6 +1,22 @@
 /* eslint no-param-reassign: "off", prefer-arrow-callback: "off" */
 
-require('babel-register')({
-  presets: ['es2015', 'react-native'],
-});
+const config = {
+  presets: ['react-native'],
+};
+
+// XX: see this for more details
+// https://github.com/lelandrichardson/react-native-mock/issues/23
+config.ignore = function babelIgnore(filename) {
+  if (!(/\/node_modules\//).test(filename)) {
+    return false; // if not in node_modules, we want to compile it
+  } else if ((/\/node_modules\/react-native.*\//).test(filename)) {
+    // its RN source code, so we want to compile it
+    return false;
+  }
+
+  // it's in node modules and NOT RN source code
+  return true;
+};
+
+require('babel-register')(config);
 require('react-native-mock/mock');
